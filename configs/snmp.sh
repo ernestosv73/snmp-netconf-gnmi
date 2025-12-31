a6d19f3ba60cae8b3f59f8593e2d7dca6be4b91c
Ernesto Sánchez <esanchez@ucasal.edu.ar>
2025-12-30 22:28:51 -0300

    Update snmp.sh

--- configs/snmp.sh
+++ configs/snmp.sh
@@ -2,20 +2,19 @@ apk update
 sleep 1
 apk add net-snmp-tools
 sleep 1
-apk add --no-cache python3 py3-pip
-sleep 1
 apk add py3-lxml
 sleep 1
 apk add --no-cache net-snmp-dev
 sleep 1
 apk add net-snmp
 sleep 1
-apk add python3-dev
-sleep 1
-apk add build-base
-sleep 1
-apk add py3-wheel
-sleep 1
 pip install easysnmp
 sleep 1
-
+apk add --no-cache libxml2 libxml2-dev
+sleep 1
+apk add --no-cache libxslt libxslt-dev libffi-dev
+sleep 1
+apk add --no-cache openssh-client
+sleep 1
+pip install ncclient
+sleep 1
